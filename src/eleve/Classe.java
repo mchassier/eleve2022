7f2c2f7359fd5736902b8690bd58df2ad28308fd
Mari <Mari@DELL-MARI.bts.sio>
2022-03-15 09:40:49 +0100

    Class Classe - getters - setters et toString créés

--- src/eleve/Classe.java
+++ src/eleve/Classe.java
@@ -8,4 +8,34 @@ public class Classe {
 		this.nbEleves = 0;
 		uneClasse = new Eleve[24];
 	}
+	
+	public int getNbEleves() {
+		return (this.nbEleves);
+	}
+	
+	public void setNbEleves(int nbEleves) {
+		this.nbEleves = nbEleves;
+	}
+
+	public Eleve[] getUneClasse() {
+		return (this.uneClasse);
+	}
+	
+	//Cette méthode sera très peu utilisée
+	public void setUneClasse(Eleve uneClasse[]) {
+		this.uneClasse = uneClasse;
+	}
+	
+	public String toString () {
+		String laClasse;
+		int indice;
+		
+		laClasse = "Liste des élèves \n";
+		for(indice=0;indice<this.nbEleves;indice++) {
+			laClasse += this.uneClasse[indice].toString()+"\n";
+		}
+		return (laClasse);
+	}
+	
+	
 }
